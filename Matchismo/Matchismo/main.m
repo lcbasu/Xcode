645bd15e9cd4a045dfcd010c71ee6339207397e3
Lokesh Basu <lokesh.basu@gmail.com>
2014-03-20 13:15:53 +0530

    Stanford Lecture Project

---
+++ Matchismo/Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Lokesh Basu on 19/03/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
