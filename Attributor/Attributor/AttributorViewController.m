6172cef81c2f3cd181a74e1b061a65af6336a708
Lokesh Basu <lokesh.basu@gmail.com>
2014-03-25 19:15:31 +0530

    Lecture 5 - NSNotification
    Showing how the change of font size in settings is captured by the
    application by tuning in to the notifications.

--- Attributor/Attributor/AttributorViewController.m
+++ Attributor/Attributor/AttributorViewController.m
@@ -30,6 +30,33 @@
     [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated]; //we can call this super before or after we are done with our work
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredFontsChanged:) name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+- (void)preferredFontsChanged:(NSNotification *)notification
+{
+    [self usePreferredFonts];
+}
+
+- (void)usePreferredFonts
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+
 - (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender
 {
     [self.body.textStorage addAttribute:NSForegroundColorAttributeName
