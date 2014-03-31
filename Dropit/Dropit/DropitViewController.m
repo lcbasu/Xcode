6bb631127d65a0b7aafccc402a7e0ecfe9b99003
Lokesh Basu <lokesh.basu@gmail.com>
2014-03-31 17:36:48 +0530

    Lecture 8 - Dropit : adding collision behaviour
    now the blocks gets stacked at the bottom and on the top of one another.

--- Dropit/Dropit/DropitViewController.m
+++ Dropit/Dropit/DropitViewController.m
@@ -12,6 +12,7 @@
 @property (weak, nonatomic) IBOutlet UIView *gameView;
 @property (strong, nonatomic) UIDynamicAnimator *animator;
 @property (strong, nonatomic) UIGravityBehavior *gravity;
+@property (strong, nonatomic) UICollisionBehavior *collider;
 @end
 
 @implementation DropitViewController
@@ -36,6 +37,16 @@ static const CGSize DROP_SIZE = { 40, 40 };
     return _gravity;
 }
 
+- (UICollisionBehavior *)collider
+{
+    if (!_collider) {
+        _collider = [[UICollisionBehavior alloc] init];
+        _collider.translatesReferenceBoundsIntoBoundary = YES;
+        [self.animator addBehavior:_collider];
+    }
+    return _collider;
+}
+
 - (IBAction)tap:(UITapGestureRecognizer *)sender
 {
     [self drop];
@@ -54,6 +65,7 @@ static const CGSize DROP_SIZE = { 40, 40 };
     [self.gameView addSubview:dropView];
     
     [self.gravity addItem:dropView];
+    [self.collider addItem:dropView];
 }
 
 - (UIColor *)randomColor
