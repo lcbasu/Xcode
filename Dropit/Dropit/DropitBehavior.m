93330f1bf540cf4427d1195f617d253bb990cd89
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-01 10:47:06 +0530

    removing the rotation behaviour

--- Dropit/Dropit/DropitBehavior.m
+++ Dropit/Dropit/DropitBehavior.m
@@ -11,6 +11,7 @@
 @interface DropitBehavior ()
 @property (strong, nonatomic) UIGravityBehavior *gravity;
 @property (strong, nonatomic) UICollisionBehavior *collider;
+@property (strong, nonatomic) UIDynamicItemBehavior *animationOptions;
 @end
 
 @implementation DropitBehavior
@@ -33,15 +34,26 @@
     return _collider;
 }
 
+- (UIDynamicItemBehavior *)animationOptions
+{
+    if (!_animationOptions) {
+        _animationOptions = [[UIDynamicItemBehavior alloc] init];
+        _animationOptions.allowsRotation = NO;
+    }
+    return _animationOptions;
+}
+
 - (void)addItem:(id <UIDynamicItem>)item
 {
     [self.gravity addItem:item];
     [self.collider addItem:item];
+    [self.animationOptions addItem:item];
 }
 - (void)removeItem:(id <UIDynamicItem>)item
 {
     [self.gravity removeItem:item];
     [self.collider removeItem:item];
+    [self.animationOptions removeItem:item];
 }
 
 - (instancetype)init
@@ -49,6 +61,7 @@
     self = [super init];
     [self addChildBehavior:self.gravity];
     [self addChildBehavior:self.collider];
+    [self addChildBehavior:self.animationOptions];
     return self;
 }
 
