d4c7cc433529951a164405d17d030c968024f9ef
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-02 21:08:32 +0530

    Lecture 12 - CoreDataExample : initial setup.

---
+++ CoreDataExample/CoreDataExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreDataExample
+//
+//  Created by Lokesh Basu on 02/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CoreDataExample/CoreDataExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataExample
+//
+//  Created by Lokesh Basu on 02/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
