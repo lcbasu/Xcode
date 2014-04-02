ef9a00cbb74be38514b7c6b3718c05fa0b247c1c
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-02 23:06:06 +0530

    Lecture 12 - CoreDataExample : categories example

---
+++ CoreDataExample/CoreDataExample/Photo+Flickr.h
@@ -0,0 +1,13 @@
+//
+//  Photo+Flickr.h
+//  CoreDataExample
+//
+//  Created by Lokesh Basu on 02/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "Photo.h"
+
+@interface Photo (Flickr)
+
+@end
---
+++ CoreDataExample/CoreDataExample/Photo+Flickr.m
@@ -0,0 +1,13 @@
+//
+//  Photo+Flickr.m
+//  CoreDataExample
+//
+//  Created by Lokesh Basu on 02/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "Photo+Flickr.h"
+
+@implementation Photo (Flickr)
+
+@end
