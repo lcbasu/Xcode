746c1aead32bc3f5ba26484bdde3053fd3b8ee7b
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-02 19:29:15 +0530

    Lecture 11 - Shutterbug : threads to process query and refresh code

--- Shutterbug/Shutterbug/JustPostedFlickrPhotosTVC.m
+++ Shutterbug/Shutterbug/JustPostedFlickrPhotosTVC.m
@@ -21,17 +21,25 @@
     [self fetchPhotos];
 }
 
-- (void)fetchPhotos
+- (IBAction)fetchPhotos
 {
-    NSURL *url = [FlickrFetcher URLforRecentGeoreferencedPhotos];
-#warning Blocks Main Thread
-    NSData *jsonResults = [NSData dataWithContentsOfURL:url];
-    NSDictionary *propertyListResults = [NSJSONSerialization JSONObjectWithData:jsonResults
-                                                                        options:0
-                                                                          error:NULL];
-//    NSLog(@"Flickr results = %@", propertyListResults);
-    NSArray *photos = [propertyListResults valueForKeyPath:FLICKR_RESULTS_PHOTOS];
-    self.photos = photos;
+    [self.refreshControl beginRefreshing];
+    NSURL *url = [FlickrFetcher URLforRecentGeoreferencedPhotos];    
+    dispatch_queue_t fetchQ = dispatch_queue_create("flickr fetcher", NULL);
+    dispatch_async(fetchQ, ^{
+        
+        NSData *jsonResults = [NSData dataWithContentsOfURL:url];
+        NSDictionary *propertyListResults = [NSJSONSerialization JSONObjectWithData:jsonResults
+                                                                            options:0
+                                                                              error:NULL];
+        NSArray *photos = [propertyListResults valueForKeyPath:FLICKR_RESULTS_PHOTOS];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.refreshControl endRefreshing];
+            self.photos = photos;
+        });
+
+    });
+    
 }
 
 @end
