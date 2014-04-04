b32d0ab68b4934fe5652fd6202d34ede94208e8d
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-04 23:36:52 +0530

    Photomania without image view controller.

--- Photomania/Photomania/PhotographersCDTVC.m
+++ Photomania/Photomania/PhotographersCDTVC.m
@@ -9,7 +9,6 @@
 #import "PhotographersCDTVC.h"
 #import "Photographer.h"
 #import "PhotoDatabaseAvailability.h"
-#import "PhotosByPhotographerCDTVC.h"
 
 @implementation PhotographersCDTVC
 
@@ -28,12 +27,11 @@
     _managedObjectContext = managedObjectContext;
     
     NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Photographer"];
-    request.predicate = nil;
+    request.predicate = nil; //predicate nil means all of them
     request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"name"
                                                               ascending:YES
                                                                selector:@selector(localizedStandardCompare:)]];
-    
-    
+//    request.fetchLimit = 100;
     
     self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:request
                                                                         managedObjectContext:managedObjectContext
@@ -48,50 +46,9 @@
     Photographer *photographer = [self.fetchedResultsController objectAtIndexPath:indexPath];
     
     cell.textLabel.text = photographer.name;
-    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d photos", (int)[photographer.photos count]];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d photos", [photographer.photos count]];
     
     return cell;
 }
-
-#pragma mark - Navigation
-
-- (void)prepareViewController:(id)vc
-                     forSegue:(NSString *)segueIdentifer
-                fromIndexPath:(NSIndexPath *)indexPath
-{
-    Photographer *photographer = [self.fetchedResultsController objectAtIndexPath:indexPath];
-    if ([vc isKindOfClass:[PhotosByPhotographerCDTVC class]]) {
-        PhotosByPhotographerCDTVC *pbpcdtvc = (PhotosByPhotographerCDTVC *)vc;
-        pbpcdtvc.photographer = photographer;
-    }
-}
-
-// boilerplate
-- (void)prepareForSegue:(UIStoryboardSegue *)segue
-                 sender:(id)sender
-{
-    NSIndexPath *indexPath = nil;
-    if ([sender isKindOfClass:[UITableViewCell class]]) {
-        indexPath = [self.tableView indexPathForCell:sender];
-    }
-    [self prepareViewController:segue.destinationViewController
-                       forSegue:segue.identifier
-                  fromIndexPath:indexPath];
-}
-
-// boilerplate
-- (void)tableView:(UITableView *)tableView
-didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    id detailvc = [self.splitViewController.viewControllers lastObject];
-    if ([detailvc isKindOfClass:[UINavigationController class]]) {
-        detailvc = [((UINavigationController *)detailvc).viewControllers firstObject];
-        [self prepareViewController:detailvc
-                           forSegue:nil
-                      fromIndexPath:indexPath];
-    }
-}
-
-
 
 @end
