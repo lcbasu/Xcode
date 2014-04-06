ec3ea946c590a907d90163973f685d4de108c5f0
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-06 14:35:59 +0530

    Lecture 14 - Photomania : IVC from Imaginarium

---
+++ Photomania/Photomania/ImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.h
+//  Imaginarium
+//
+//  Created by Lokesh Basu on 02/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@property (strong, nonatomic) NSURL *imageURL;
+
+@end
---
+++ Photomania/Photomania/ImageViewController.m
@@ -0,0 +1,122 @@
+//
+//  ImageViewController.m
+//  Imaginarium
+//
+//  Created by Lokesh Basu on 02/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "ImageViewController.h"
+
+@interface ImageViewController () <UIScrollViewDelegate, UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) UIImageView *imageView;
+@property (strong, nonatomic) UIImage *image;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+
+@end
+
+@implementation ImageViewController
+
+- (void)setScrollView:(UIScrollView *)scrollView
+{
+    _scrollView = scrollView;
+    _scrollView.minimumZoomScale = 0.2;
+    _scrollView.maximumZoomScale = 2.0;
+    _scrollView.delegate = self;
+    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
+}
+
+- (void)setImageURL:(NSURL *)imageURL
+{
+    _imageURL = imageURL;
+//    self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:self.imageURL]];
+    [self startDownloadingImage];
+}
+
+- (void)startDownloadingImage
+{
+    self.image  =nil;
+    if (self.imageURL) {
+        [self.spinner startAnimating];
+        NSURLRequest *request = [NSURLRequest requestWithURL:self.imageURL];
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+        NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request
+                        completionHandler:^(NSURL *localfile, NSURLResponse *response, NSError *error) {
+                            if (!error) {
+                                if ([request.URL isEqual:self.imageURL]) {
+                                    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:localfile]];
+                                    dispatch_async(dispatch_get_main_queue(), ^{ self.image = image; });
+                                }
+                            }
+                        }];
+        [task resume];
+    }
+}
+
+#pragma mark - UISplitViewControllerDelegate
+
+- (void)awakeFromNib
+{
+    self.splitViewController.delegate = self;
+}
+
+- (BOOL)splitViewController:(UISplitViewController *)svc
+   shouldHideViewController:(UIViewController *)vc
+              inOrientation:(UIInterfaceOrientation)orientation
+{
+    return UIInterfaceOrientationIsPortrait(orientation);
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+     willHideViewController:(UIViewController *)aViewController
+          withBarButtonItem:(UIBarButtonItem *)barButtonItem
+       forPopoverController:(UIPopoverController *)pc
+{
+    barButtonItem.title = aViewController.title;
+    self.navigationItem.leftBarButtonItem = barButtonItem;
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+     willShowViewController:(UIViewController *)aViewController
+  invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem
+{
+    self.navigationItem.leftBarButtonItem = nil;
+}
+
+
+- (UIImageView *)imageView
+{
+    if (!_imageView) _imageView = [[UIImageView alloc] init];
+    return _imageView;
+}
+
+- (UIImage *)image
+{
+    return self.imageView.image;
+}
+
+- (void)setImage:(UIImage *)image
+{
+    self.scrollView.zoomScale = 1.0;
+    self.imageView.image = image;
+    [self.imageView sizeToFit];
+    self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+    [self.spinner stopAnimating];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.scrollView addSubview:self.imageView];
+}
+
+@end
