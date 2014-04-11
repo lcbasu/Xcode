91f9526a56f24efdd3385fd18dd59e218b4fb301
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-11 15:29:35 +0530

    Lecture 15 - Photomania : MapKit demo setup

---
+++ Photomania/Photomania/PhotosByPhotographerMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosByPhotographerMapViewController.h
+//  Photomania
+//
+//  Created by Lokesh Basu on 11/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosByPhotographerMapViewController : UIViewController
+
+@end
