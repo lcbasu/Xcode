12430a76503a7312148886e4d6116a4f9decce09
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-12 09:48:43 +0530

    Lecture 15 - Photomania : including MapKit framework

--- Photomania/Photomania/PhotosByPhotographerMapViewController.h
+++ Photomania/Photomania/PhotosByPhotographerMapViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Photographer.h"
 
 @interface PhotosByPhotographerMapViewController : UIViewController
+
+@property (nonatomic, strong) Photographer *photographer;
 
 @end
