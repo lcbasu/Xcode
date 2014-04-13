85552145a6e380725e2bd6fbe4323c8a9b7ba10d
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-13 10:18:02 +0530

    Command Line Tool tutorial setup

---
+++ CLT Project/CLT Project/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CLT Project
+//
+//  Created by Lokesh Basu on 13/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
