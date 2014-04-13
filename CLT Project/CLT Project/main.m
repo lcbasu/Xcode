d02755dfc40a22f10efe498623477024f2d442c4
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-13 11:03:19 +0530

    CLT game.

--- CLT Project/CLT Project/main.m
+++ CLT Project/CLT Project/main.m
@@ -10,13 +10,31 @@
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
+        int answer = 0;
+        int guess = 0;
+        int turn = 0;
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        answer = arc4random() % 100 + 1;
         
+        while (guess != answer) {
+            turn++;
+            
+            NSLog(@"Guess #%i: Enter a number between 1 and 100", turn);
+            scanf("%i", &guess);
+            
+            if (guess > answer) {
+                NSLog(@"Lower!");
+            }
+            else if (guess < answer) {
+                NSLog(@"Higher!");
+            }
+            else {
+                NSLog(@"Correct! The answer was %i", answer);
+            }
+        } // end of while loop
+        
+        NSLog(@"It took you %i tries", turn);
     }
     return 0;
 }
-
