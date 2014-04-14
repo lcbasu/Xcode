feb9c141f86be074fa38c4a7c789d34e3080f896
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-14 09:50:07 +0530

    PeopleDatabase project setup.

---
+++ PeopleDatabase/PeopleDatabase/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PeopleDatabase
+//
+//  Created by Lokesh Basu on 14/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
