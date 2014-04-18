e2869f8f55350b81fd249d7f48375e2af5e25c5c
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-18 11:29:30 +0530

    converting image into colour pattern

--- TapMe/TapMe/ViewController.m
+++ TapMe/TapMe/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    self.view.backgroundColor = [UIColor purpleColor];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_tile.png"]];
     
     //  setup and start the game
     [self setupGame];
