bd7fc63fa0a6f991ca3a43a5753883774a2e9bee
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-19 11:40:34 +0530

    Vehicles project setup

---
+++ Vehicles/Vehicles/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Vehicles
+//
+//  Created by Lokesh Basu on 19/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Vehicles/VehiclesTests/VehiclesTests.m
@@ -0,0 +1,34 @@
+//
+//  VehiclesTests.m
+//  VehiclesTests
+//
+//  Created by Lokesh Basu on 19/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VehiclesTests : XCTestCase
+
+@end
+
+@implementation VehiclesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
