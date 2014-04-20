b4f5f7ca0461693112a4fe0bd046e500a6a4297c
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-20 13:47:07 +0530

    starting a weather app project.

---
+++ SimpleWeather/SimpleWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by Lokesh Basu on 20/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
