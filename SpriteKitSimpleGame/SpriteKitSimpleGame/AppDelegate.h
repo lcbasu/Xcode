3f945921c472334a64a1189bf4567b2d95f17dfc
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-21 17:23:23 +0530

    Sprite Kit beginner tutorial project setup.

---
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitSimpleGame
+//
+//  Created by Lokesh Basu on 21/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitSimpleGame/SpriteKitSimpleGameTests/SpriteKitSimpleGameTests.m
@@ -0,0 +1,34 @@
+//
+//  SpriteKitSimpleGameTests.m
+//  SpriteKitSimpleGameTests
+//
+//  Created by Lokesh Basu on 21/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpriteKitSimpleGameTests : XCTestCase
+
+@end
+
+@implementation SpriteKitSimpleGameTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
