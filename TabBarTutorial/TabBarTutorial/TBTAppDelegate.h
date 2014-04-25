fe860faa043c3984b303ca1665f571c29e206c6c
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-25 10:24:10 +0530

    setup a tab bar tutorial project.

---
+++ TabBarTutorial/TabBarTutorial/TBTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBTAppDelegate.h
+//  TabBarTutorial
+//
+//  Created by Lokesh Basu on 25/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TabBarTutorial/TabBarTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TabBarTutorial
+//
+//  Created by Lokesh Basu on 25/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TBTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBTAppDelegate class]));
+    }
+}
