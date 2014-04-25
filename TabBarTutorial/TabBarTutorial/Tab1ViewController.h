6c71b5192a5ddb8d5caed3bafcb1cb8161285644
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-25 10:28:43 +0530

    refactoring to give more descriptive name.

---
+++ TabBarTutorial/TabBarTutorial/Tab1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  TBTViewController.h
+//  TabBarTutorial
+//
+//  Created by Lokesh Basu on 25/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Tab1ViewController : UIViewController
+
+@end
---
+++ TabBarTutorial/TabBarTutorial/Tab1ViewController.m
@@ -0,0 +1,29 @@
+//
+//  TBTViewController.m
+//  TabBarTutorial
+//
+//  Created by Lokesh Basu on 25/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "Tab1ViewController.h"
+
+@interface Tab1ViewController ()
+
+@end
+
+@implementation Tab1ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
