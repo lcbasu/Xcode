b37b685625140c430219505337e50995d9841caa
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-25 10:31:52 +0530

    adding the View Controller for the second content view

---
+++ TabBarTutorial/TabBarTutorial/Tab2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Tab2ViewController.h
+//  TabBarTutorial
+//
+//  Created by Lokesh Basu on 25/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Tab2ViewController : UIViewController
+
+@end
---
+++ TabBarTutorial/TabBarTutorial/Tab2ViewController.m
@@ -0,0 +1,49 @@
+//
+//  Tab2ViewController.m
+//  TabBarTutorial
+//
+//  Created by Lokesh Basu on 25/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "Tab2ViewController.h"
+
+@interface Tab2ViewController ()
+
+@end
+
+@implementation Tab2ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
