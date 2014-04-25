a3fce4c0fd000ae1055f5fff34e422c883181385
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-25 12:17:41 +0530

    swipeable Table View Cell project setup

---
+++ SwipeableCell/SwipeableCell/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  SwipeableCell
+//
+//  Created by Lokesh Basu on 25/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
