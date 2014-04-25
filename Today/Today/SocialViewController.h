a18187f3c0c61cc84813b6e3f085faf32b483557
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-25 10:58:37 +0530

    adding View Controller for social notifications.

---
+++ Today/Today/SocialViewController.h
@@ -0,0 +1,13 @@
+//
+//  SocialViewController.h
+//  Today
+//
+//  Created by Lokesh Basu on 25/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocialViewController : UIViewController
+
+@end
