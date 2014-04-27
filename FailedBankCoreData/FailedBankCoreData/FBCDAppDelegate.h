0bba068cf78c14b0c32cae2ed810bf5306b50ae4
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-27 11:21:25 +0530

    core data tutorial setup.

---
+++ FailedBankCoreData/FailedBankCoreData/FBCDAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  FBCDAppDelegate.h
+//  FailedBankCoreData
+//
+//  Created by Lokesh Basu on 27/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBCDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
