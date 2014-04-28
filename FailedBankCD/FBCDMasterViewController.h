d7a0ddb3332746d0c9d477832c6be92dfaf59023
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-28 11:46:26 +0530

    creating new Table View classes

---
+++ FailedBankCD/FBCDMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBCDMasterViewController.h
+//  FailedBankCD
+//
+//  Created by Lokesh Basu on 28/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBCDMasterViewController : UITableViewController
+
+@end
