522bc1875af55dac7626b826bea8d7256bd9c86f
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-30 16:32:51 +0530

    instantiating Car from Vehicle

---
+++ Vehicles/Vehicles/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Lokesh Basu on 30/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@end
---
+++ Vehicles/Vehicles/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  Vehicles
+//
+//  Created by Lokesh Basu on 30/04/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
