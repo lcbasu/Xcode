19a6fd143ab639d0e448b3c2850de99eaf000008
Lokesh Basu <lokesh.basu@gmail.com>
2014-04-30 16:46:57 +0530

    superclass overrides

--- Vehicles/Vehicles/Car.m
+++ Vehicles/Vehicles/Car.m
@@ -21,9 +21,33 @@
 }
 
 #pragma mark - Private method to start the Car
+
 - (NSString *)start
 {
     return [NSString stringWithFormat:@"Start power source %@.", self.powerSource];
+}
+
+
+#pragma mark - Superclass Overrides
+
+- (NSString *)goForward
+{
+    return [NSString stringWithFormat:@"%@ %@ Then depress gas pedal.", [self start], [self changeGears:@"Forward"]];
+}
+
+- (NSString *)goBackward
+{
+    return [NSString stringWithFormat:@"%@ %@ Check your rear view mirror. Then depress gas pedal.", [self start], [self changeGears:@"Reverse"]];
+}
+
+- (NSString *)stopMoving
+{
+    return [NSString stringWithFormat:@"Depress brake pedal. %@", [self changeGears:@"Park"]];
+}
+
+- (NSString *)makeNoise
+{
+    return @"Beep beep!";
 }
 
 @end
