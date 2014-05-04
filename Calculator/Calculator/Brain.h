4b888b096ca295a969dc152c70476d66fd707aca
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-04 09:43:43 +0530

    complete interface and main computation file

---
+++ Calculator/Calculator/Brain.h
@@ -0,0 +1,13 @@
+//
+//  Brain.h
+//  Calculator
+//
+//  Created by Lokesh Basu on 04/05/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Brain : NSObject
+
+@end
---
+++ Calculator/Calculator/Brain.m
@@ -0,0 +1,13 @@
+//
+//  Brain.m
+//  Calculator
+//
+//  Created by Lokesh Basu on 04/05/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "Brain.h"
+
+@implementation Brain
+
+@end
