05f5b2d4ddc5a9cad22689b1703654c2a37ac87f
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-04 07:27:12 +0530

    setup project for OS X to edit song detail

---
+++ SongDetail/SongDetail/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  SongDetail
+//
+//  Created by Lokesh Basu on 04/05/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
