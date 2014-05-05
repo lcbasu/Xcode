450a218b76359f8e038d486fb328b9b5626c8692
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-05 10:23:35 +0530

    project setup for stopwatch application

---
+++ Stopwatch/Stopwatch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Stopwatch
+//
+//  Created by Lokesh Basu on 05/05/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
