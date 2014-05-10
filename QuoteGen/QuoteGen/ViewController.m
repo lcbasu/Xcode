61615e1c8da01be75c4abd6f4242969384e116ec
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-10 11:06:42 +0530

    quotes in case of different selection of option

--- QuoteGen/QuoteGen/ViewController.m
+++ QuoteGen/QuoteGen/ViewController.m
@@ -49,6 +49,28 @@
         NSString *my_quote = self.myQuotes[index];
         self.quoteText.text = [NSString stringWithFormat:@"Quote:\n\n%@",  my_quote];
     }
+    else {
+        
+        NSString *selectedCategory = @"classic";
+        
+        if (self.quoteOpt.selectedSegmentIndex == 1) {
+            selectedCategory = @"modern";
+        }
+        
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"category == %@", selectedCategory];
+        NSArray *filteredArray = [self.movieQuotes filteredArrayUsingPredicate:predicate];
+        
+        int array_tot = [filteredArray count];
+        
+        if (array_tot > 0) {
+
+            int index = (arc4random() % array_tot);
+            NSString *quote = filteredArray[index][@"quote"];
+            self.quoteText.text = [NSString stringWithFormat:@"Movie Quote:\n\n%@",  quote];
+        } else {
+            self.quoteText.text = [NSString stringWithFormat:@"No quotes to display."];
+        }
+    }
 }
 
 @end
