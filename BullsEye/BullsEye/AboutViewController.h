e03a1250c582657905f884684bd754ae974aa7c6
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-15 07:23:39 +0530

    modal view for description about the app

---
+++ BullsEye/BullsEye/AboutViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Lokesh Basu on 15/05/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+- (IBAction)close;
+
+@end
