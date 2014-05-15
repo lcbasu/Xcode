926086795202a80912cd1bfce98678543e8c1863
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-15 08:13:33 +0530

    changes graphics of slider

--- BullsEye/BullsEye/BullsEyeViewController.m
+++ BullsEye/BullsEye/BullsEyeViewController.m
@@ -25,6 +25,21 @@
     [super viewDidLoad];
     [self startNewGame];
     [self updateLabels];
+    
+    //change graphics of the slider
+    
+    UIImage *thumbImageNormal = [UIImage imageNamed:@"SliderThumb-Normal"];
+    [self.slider setThumbImage:thumbImageNormal forState:UIControlStateNormal];
+    
+    UIImage *thumbImageHighlighted = [UIImage imageNamed:@"SliderThumb-Highlighted"];
+    [self.slider setThumbImage:thumbImageHighlighted forState:UIControlStateHighlighted];
+    
+    UIImage *trackLeftImage = [[UIImage imageNamed:@"SliderTrackLeft"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 14, 0, 14)];
+    [self.slider setMinimumTrackImage:trackLeftImage forState:UIControlStateNormal];
+    
+    UIImage *trackRightImage = [[UIImage imageNamed:@"SliderTrackRight"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 14, 0, 14)];
+    [self.slider setMaximumTrackImage:trackRightImage forState:UIControlStateNormal];
+    
 }
 
 - (void)startNewRound
@@ -73,7 +88,7 @@
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title
                                                         message:message
                                                        delegate:self
-                                              cancelButtonTitle:@"Cancel"
+                                              cancelButtonTitle:@"Next Round"
                                               otherButtonTitles:nil];    
     [alertView show];
 }
