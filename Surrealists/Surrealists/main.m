77893901530062c3d7546578c7c781b66b633c65
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-16 16:25:48 +0530

    Boom! App project setup

---
+++ Surrealists/Surrealists/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Surrealists
+//
+//  Created by Lokesh Basu on 16/05/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
