2716b219a707b6bbd23baafeeae325ed8f3cb88c
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-17 06:51:22 +0530

    TextKit tutorial starter project

---
+++ TextKitNotepad/TextKitNotepad/NoteEditorViewController.m
@@ -0,0 +1,37 @@
+//
+//  CENoteEditorControllerViewController.m
+//  TextKitNotepad
+//
+//  Created by Colin Eberhardt on 19/06/2013.
+//  Copyright (c) 2013 Colin Eberhardt. All rights reserved.
+//
+
+#import "NoteEditorViewController.h"
+#import "Note.h"
+
+@interface NoteEditorViewController () <UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@end
+
+@implementation NoteEditorViewController
+{
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.textView.text = self.note.contents;
+    self.textView.delegate = self;
+    self.textView.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+}
+
+- (void)textViewDidEndEditing:(UITextView *)textView
+{
+    // copy the updated note text to the underlying model.
+    _note.contents = textView.text;
+}
+
+@end
