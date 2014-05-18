32ee00df73bb409d6fd4db1dae9c92920ad85eab
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-18 19:19:57 +0530

    project setup for chapter: Defining Classes

---
+++ DefiningClasses/DefiningClasses/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DefiningClasses
+//
+//  Created by Lokesh Basu on 18/05/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
