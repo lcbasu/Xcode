1c57791715ecd9112e82a651eee6004b4cd180fd
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-20 11:48:40 +0530

    adds classes for TVC and VC

---
+++ CoreDataTutorial/CoreDataTutorial/CDTTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDTTableViewController.h
+//  CoreDataTutorial
+//
+//  Created by Lokesh Basu on 20/05/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDTTableViewController : UITableViewController
+
+@end
