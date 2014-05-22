860dd4144ac72d795b24e68d2f3a24e600c5d7ee
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-22 10:31:47 +0530

    project setup for GCD tutorial

---
+++ SlowWorker/SlowWorker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlowWorker
+//
+//  Created by Lokesh Basu on 22/05/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
