abd5403095193a26ff6be6847c30695a213b57d3
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-22 10:02:47 +0530

    project setup for Collections tutorial

---
+++ Collections/Collections/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Collections
+//
+//  Created by Lokesh Basu on 22/05/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
