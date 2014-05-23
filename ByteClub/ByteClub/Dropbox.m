fec6f8c10642008b19fd1a5a5ce0cc7197abc424
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-23 09:54:07 +0530

    setup the api keys, secret and folder

--- ByteClub/ByteClub/Dropbox.m
+++ ByteClub/ByteClub/Dropbox.m
@@ -8,12 +8,10 @@
 
 #import "Dropbox.h"
 
-#warning INSERT YOUR OWN API KEY and SECRET HERE
-static NSString *apiKey = @"YOUR_KEY";
-static NSString *appSecret = @"YOUR_SECRET";
+static NSString *apiKey = @"39kq206nwcm68p6";
+static NSString *appSecret = @"f97e2hermmo0gia";
 
-#warning THIS FOLDER MUST BE CREATED AT THE TOP LEVEL OF YOUR DROPBOX FOLDER, you can then share this folder with others
-NSString * const appFolder = @"byteclub";
+NSString * const appFolder = @"basubyteclub";
 
 NSString * const oauthTokenKey = @"oauth_token";
 NSString * const oauthTokenKeySecret = @"oauth_token_secret";
