19f9acc348e5610f61177ba1d4548354afa9813b
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-31 16:45:59 +0530

    custom URL loading

--- NSURLProtocolExample/NSURLProtocolExample/MyURLProtocol.m
+++ NSURLProtocolExample/NSURLProtocolExample/MyURLProtocol.m
@@ -8,13 +8,54 @@
 
 #import "MyURLProtocol.h"
 
+@interface MyURLProtocol () <NSURLConnectionDelegate>
+@property (nonatomic, strong) NSURLConnection *connection;
+@end
+
 @implementation MyURLProtocol
 
 + (BOOL)canInitWithRequest:(NSURLRequest *)request
 {
     static NSUInteger requestCount = 0;
     NSLog(@"Request #%u: URL = %@", requestCount++, request.URL.absoluteString);
-    return NO;
+    return YES;
+}
+
+#pragma mark - connection delegate methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [self.client URLProtocol:self didLoadData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    [self.client URLProtocolDidFinishLoading:self];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    [self.client URLProtocol:self didFailWithError:error];
+}
+
+#pragma mark - request methods
+
++ (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request {
+    return request;
+}
+
++ (BOOL)requestIsCacheEquivalent:(NSURLRequest *)a toRequest:(NSURLRequest *)b {
+    return [super requestIsCacheEquivalent:a toRequest:b];
+}
+
+- (void)startLoading {
+    self.connection = [NSURLConnection connectionWithRequest:self.request delegate:self];
+}
+
+- (void)stopLoading {
+    [self.connection cancel];
+    self.connection = nil;
 }
 
 @end
