144ddbd3feb5c4bb22a7077672861013e0a2f1e3
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-31 08:09:59 +0530

    blocks tutorial project setup

---
+++ iOSDiner/iOSDiner/IODViewController.h
@@ -0,0 +1,31 @@
+//
+//  IODViewController.h
+//  iOSDiner
+//
+//  Created by Adam Burkepile on 1/29/12.
+//  Copyright (c) 2012 Adam Burkepile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IODViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *ibRemoveItemButton;
+@property (weak, nonatomic) IBOutlet UIButton *ibAddItemButton;
+@property (weak, nonatomic) IBOutlet UIButton *ibPreviousItemButton;
+@property (weak, nonatomic) IBOutlet UIButton *ibNextItemButton;
+@property (weak, nonatomic) IBOutlet UIButton *ibTotalOrderButton;
+@property (weak, nonatomic) IBOutlet UILabel *ibChalkboardLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *ibCurrentItemImageView;
+@property (weak, nonatomic) IBOutlet UILabel *ibCurrentItemLabel;
+
+- (IBAction)ibaRemoveItem:(id)sender;
+- (IBAction)ibaAddItem:(id)sender;
+- (IBAction)ibaLoadPreviousItem:(id)sender;
+- (IBAction)ibaLoadNextItem:(id)sender;
+- (IBAction)ibaCalculateTotal:(id)sender;
+
+
+
+
+@end
---
+++ iOSDiner/iOSDiner/IODViewController.m
@@ -0,0 +1,90 @@
+//
+//  IODViewController.m
+//  iOSDiner
+//
+//  Created by Adam Burkepile on 1/29/12.
+//  Copyright (c) 2012 Adam Burkepile. All rights reserved.
+//
+
+#import "IODViewController.h"
+
+@implementation IODViewController
+@synthesize ibRemoveItemButton;
+@synthesize ibAddItemButton;
+@synthesize ibPreviousItemButton;
+@synthesize ibNextItemButton;
+@synthesize ibTotalOrderButton;
+@synthesize ibChalkboardLabel;
+@synthesize ibCurrentItemImageView;
+@synthesize ibCurrentItemLabel;
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [self setIbRemoveItemButton:nil];
+    [self setIbAddItemButton:nil];
+    [self setIbPreviousItemButton:nil];
+    [self setIbNextItemButton:nil];
+    [self setIbTotalOrderButton:nil];
+    [self setIbChalkboardLabel:nil];
+    [self setIbCurrentItemImageView:nil];
+    [self setIbCurrentItemLabel:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)ibaRemoveItem:(id)sender {
+}
+
+- (IBAction)ibaAddItem:(id)sender {
+}
+
+- (IBAction)ibaLoadPreviousItem:(id)sender {
+}
+
+- (IBAction)ibaLoadNextItem:(id)sender {
+}
+
+- (IBAction)ibaCalculateTotal:(id)sender {
+}
+@end
