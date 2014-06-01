610a0691a2f95bc85cb13ab17d2aa4f124667947
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-01 10:31:02 +0530

    adds OAuth authentication

--- FBParse/FBParse/AppDelegate.m
+++ FBParse/FBParse/AppDelegate.m
@@ -9,7 +9,22 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // register the parse application
+    [Parse setApplicationId:@"SCi48V6V3sPq8qfrnnbaBmbQudYgJouhpaMkRhdk" clientKey:@"qkYCHnq0ahWXFQunpO00Jx3Rddtay5qNdKy3Ecj0"];
+    
+    // initialize Parse's Facebook Utilities singleton
+    [PFFacebookUtils initializeFacebook];
 	return YES;
+}
+
+- (BOOL) application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    return [PFFacebookUtils handleOpenURL:url];
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    return [PFFacebookUtils handleOpenURL:url];
 }
 
 @end
