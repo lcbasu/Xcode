710cd5f87ee98e10030ff854d46b5fcc8caea384
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-01 10:36:05 +0530

    creates class for communications

---
+++ FBParse/FBParse/Comms.h
@@ -0,0 +1,13 @@
+//
+//  Comms.h
+//  FBParse
+//
+//  Created by Lokesh Basu on 01/06/14.
+//  Copyright (c) 2014 Toby Stephens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Comms : NSObject
+
+@end
---
+++ FBParse/FBParse/Comms.m
@@ -0,0 +1,13 @@
+//
+//  Comms.m
+//  FBParse
+//
+//  Created by Lokesh Basu on 01/06/14.
+//  Copyright (c) 2014 Toby Stephens. All rights reserved.
+//
+
+#import "Comms.h"
+
+@implementation Comms
+
+@end
