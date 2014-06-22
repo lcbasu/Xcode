c190e3297d22f850773020121e7bc290a61b25c6
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-22 16:59:36 +0530

    views for uploader and receiver of the Ticket

---
+++ TicketX/TicketX/TXHasTicketViewController.h
@@ -0,0 +1,13 @@
+//
+//  TXHasTicketViewController.h
+//  TicketX
+//
+//  Created by Lokesh Basu on 22/06/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TXHasTicketViewController : UIViewController
+
+@end
---
+++ TicketX/TicketX/TXWantTicketViewController.h
@@ -0,0 +1,13 @@
+//
+//  TXWantTicketViewController.h
+//  TicketX
+//
+//  Created by Lokesh Basu on 22/06/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TXWantTicketViewController : UIViewController
+
+@end
