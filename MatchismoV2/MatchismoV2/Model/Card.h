e3d6eea57b5fecb2e6fc869fda6a1078fecd0d43
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-23 19:06:44 +0530

    Creates Model with Card as initial class

---
+++ MatchismoV2/MatchismoV2/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  MatchismoV2
+//
+//  Created by Lokesh Basu on 23/06/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ MatchismoV2/MatchismoV2/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  MatchismoV2
+//
+//  Created by Lokesh Basu on 23/06/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
