a3094932ac2c120f4f42bc1486fe7c241187717c
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-23 13:31:56 +0530

    initial setup for Matchismo Version 2

---
+++ MatchismoV2/MatchismoV2/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  MatchismoV2
+//
+//  Created by Lokesh Basu on 23/06/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
