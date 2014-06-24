2d86ed1f8bc76c2ce08103179baee5e530b048e9
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-24 14:44:34 +0530

    new class for card matching game Model

---
+++ MatchismoV2/MatchismoV2/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  MatchismoV2
+//
+//  Created by Lokesh Basu on 24/06/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ MatchismoV2/MatchismoV2/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  MatchismoV2
+//
+//  Created by Lokesh Basu on 24/06/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
