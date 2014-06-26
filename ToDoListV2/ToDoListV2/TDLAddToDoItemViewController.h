082d92df2bbe59cf2687f67fa68ae9fc85525306
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-26 13:59:23 +0530

    custom view controllers for all the views

---
+++ ToDoListV2/ToDoListV2/TDLAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  TDLAddToDoItemViewController.h
+//  ToDoListV2
+//
+//  Created by Lokesh Basu on 26/06/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLAddToDoItemViewController : UIViewController
+
+@end
---
+++ ToDoListV2/ToDoListV2/TDLToDoListViewController.h
@@ -0,0 +1,13 @@
+//
+//  TDLToDoListViewController.h
+//  ToDoListV2
+//
+//  Created by Lokesh Basu on 26/06/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLToDoListViewController : UITableViewController
+
+@end
