aeb8c12fc66ffaa01b73058e9b2a154a72ad65e0
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-28 11:29:47 +0530

    cleans up the unnecessary code

--- Button Fun/Button Fun/BIDViewController.m
+++ Button Fun/Button Fun/BIDViewController.m
@@ -8,22 +8,6 @@
 
 #import "BIDViewController.h"
 
-@interface BIDViewController ()
-
-@end
-
 @implementation BIDViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
