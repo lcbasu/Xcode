1219e25c64213ff2f86ad43c8fa8a39397496b76
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-29 11:16:52 +0530

    adds bold font attribute

--- Button Fun/Button Fun/BIDViewController.m
+++ Button Fun/Button Fun/BIDViewController.m
@@ -15,5 +15,12 @@
     NSString *buttonTitle = [sender titleForState:UIControlStateNormal];
     NSString *labelText = [NSString stringWithFormat:@"%@ button pressed.", buttonTitle];
     _statusLabel.text = labelText;
+    
+    // styling the text
+    NSMutableAttributedString *styledtext = [[NSMutableAttributedString alloc] initWithString:labelText];
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:_statusLabel.font.pointSize]};
+    NSRange nameRange = [labelText rangeOfString:buttonTitle];
+    [styledtext setAttributes:attributes range:nameRange];
+    _statusLabel.attributedText = styledtext;
 }
 @end
