f21efaa269f94ccb8c560b2c26fb2ad537687d3a
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-04 14:02:36 +0530

    initial project setup for chapter 5 of BID book.

---
+++ Orientations/Orientations/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Orientations
+//
+//  Created by Lokesh Basu on 04/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ Orientations/OrientationsTests/OrientationsTests.m
@@ -0,0 +1,34 @@
+//
+//  OrientationsTests.m
+//  OrientationsTests
+//
+//  Created by Lokesh Basu on 04/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OrientationsTests : XCTestCase
+
+@end
+
+@implementation OrientationsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
