b9c57da69401c568cde2b802e2a9be528cb532a1
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-04 14:37:25 +0530

    initial setup for Autosize project

---
+++ Autosize/Autosize/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Autosize
+//
+//  Created by Lokesh Basu on 04/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
