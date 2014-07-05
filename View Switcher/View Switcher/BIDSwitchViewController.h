5c5bf1aaf396711b938832ae7eccd0e5b545435c
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-05 16:32:52 +0530

    Create our View Controller and Storyboard

---
+++ View Switcher/View Switcher/BIDSwitchViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDSwitchViewController.h
+//  View Switcher
+//
+//  Created by Lokesh Basu on 05/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDSwitchViewController : UIViewController
+
+@end
---
+++ View Switcher/View Switcher/BIDYellowViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDYellowViewController.h
+//  View Switcher
+//
+//  Created by Lokesh Basu on 05/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDYellowViewController : UIViewController
+
+@end
