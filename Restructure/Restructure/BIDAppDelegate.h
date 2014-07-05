e648bad61dab7ed8df9b97281828357e1d238c10
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-05 13:17:47 +0530

    initial setup for Restructure project

---
+++ Restructure/Restructure/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Restructure
+//
+//  Created by Lokesh Basu on 05/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Restructure/Restructure/BIDViewController.m
@@ -0,0 +1,29 @@
+//
+//  BIDViewController.m
+//  Restructure
+//
+//  Created by Lokesh Basu on 05/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import "BIDViewController.h"
+
+@interface BIDViewController ()
+
+@end
+
+@implementation BIDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
