c24a8b536f29ac98b9afaaed4c20c1523523657d
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-06 15:58:06 +0530

    initial project setup for Pickers

---
+++ Pickers/Pickers/BIDAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  BIDAppDelegate.h
+//  Pickers
+//
+//  Created by Lokesh Basu on 06/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Pickers/PickersTests/PickersTests.m
@@ -0,0 +1,34 @@
+//
+//  PickersTests.m
+//  PickersTests
+//
+//  Created by Lokesh Basu on 06/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PickersTests : XCTestCase
+
+@end
+
+@implementation PickersTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
