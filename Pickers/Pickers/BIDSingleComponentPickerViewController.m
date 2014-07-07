2d5321d5157a1433fd5ab501ddceeec200733a25
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-07 09:17:57 +0530

    completes single component picker

--- Pickers/Pickers/BIDSingleComponentPickerViewController.m
+++ Pickers/Pickers/BIDSingleComponentPickerViewController.m
@@ -11,6 +11,7 @@
 @interface BIDSingleComponentPickerViewController ()
 
 @property (weak, nonatomic) IBOutlet UIPickerView *singlePicker;
+@property (strong, nonatomic) NSArray *characterNames;
 
 @end
 
@@ -28,7 +29,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.characterNames = @[@"Luke", @"Leia", @"Han", @"Chewbacca", @"Artoo", @"Threepio", @"Lando"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -50,6 +51,35 @@
 
 - (IBAction)buttonPressed:(UIButton *)sender
 {
+    NSInteger row = [self.singlePicker selectedRowInComponent:0];
+    NSString *selected = self.characterNames[row];
+    NSString *title = [[NSString alloc] initWithFormat:
+                       @"You selected %@!", selected];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                    message:@"Thank you for choosing."
+                                                   delegate:nil
+                                          cancelButtonTitle:@"You're Welcome"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
+#pragma mark Picker Data Source Methods
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [self.characterNames count];
+}
+
+#pragma mark Picker Data Source Methods
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return self.characterNames[row];
 }
 
 @end
