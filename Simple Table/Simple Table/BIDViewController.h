35180e290b93d1327e2f3f00344a1a717f5df616
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-07 12:08:53 +0530

    initial commit for Simple Table

---
+++ Simple Table/Simple Table/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Simple Table
+//
+//  Created by Lokesh Basu on 07/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ Simple Table/Simple TableTests/Simple_TableTests.m
@@ -0,0 +1,34 @@
+//
+//  Simple_TableTests.m
+//  Simple TableTests
+//
+//  Created by Lokesh Basu on 07/07/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Simple_TableTests : XCTestCase
+
+@end
+
+@implementation Simple_TableTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
