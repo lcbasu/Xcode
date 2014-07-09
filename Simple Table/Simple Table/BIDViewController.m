b6fe7fa09fbaafa1e012dd1a4f499cf9e34cc956
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-09 13:14:23 +0530

    set up the controller

--- Simple Table/Simple Table/BIDViewController.m
+++ Simple Table/Simple Table/BIDViewController.m
@@ -10,6 +10,8 @@
 
 @interface BIDViewController ()
 
+@property (copy, nonatomic) NSArray *dwarves;
+
 @end
 
 @implementation BIDViewController
@@ -17,13 +19,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.dwarves = @[@"Sleepy", @"Sneezy", @"Bashful", @"Happy", @"Doc", @"Grumpy", @"Dopey", @"Thorin", @"Dorin", @"Nori", @"Ori", @"Balin", @"Dwalin", @"Fili", @"Kili", @"Oin", @"Gloin", @"Bifur", @"Bofur", @"Bombur"];
+    UITableView *tableView = (id)[self.view viewWithTag:1];
+    UIEdgeInsets contentInset = tableView.contentInset;
+    contentInset.top = 20;
+    [tableView setContentInset:contentInset];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.dwarves count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *SimpleTableIdentifier = @"SimpleTableIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:
+                             SimpleTableIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleDefault
+                reuseIdentifier:SimpleTableIdentifier];
+    }
+    cell.textLabel.text = self.dwarves[indexPath.row];
+    return cell;
 }
 
 @end
