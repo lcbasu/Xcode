b9bf7019655ed318719e80a1088663b509b12c11
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-21 18:39:12 +0530

    normal calculations

--- SwiftTutorial.playground/section-1.swift
+++ SwiftTutorial.playground/section-1.swift
@@ -2,4 +2,21 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+let tutorialTeam = 56
+let editorialTeam = 23
+let totalTeam = tutorialTeam + editorialTeam
+
+let priceInferred = 19.99
+let priceExplicit: Double = 19.99
+
+let onSaleInferred = true
+let onSaleExplicit: Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit: String = "Whoopie Cushion"
+
+if onSaleInferred {
+    println("\(nameInferred) on sale for \(priceInferred)!")
+} else {
+    println("\(nameInferred) at regular price: \(priceInferred)!")
+}
