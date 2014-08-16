08eb6193e89015675364be8d015d041ea75f007d
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-16 09:56:18 +0530

    project setup for OpenGL

---
+++ OpenGL/OpenGL/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Lokesh Basu on 16/08/14.
+//  Copyright (c) 2014 Samsung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
