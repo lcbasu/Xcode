ef95257454abb640f39cba45eeb0815b98d058d9
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-17 14:26:30 +0530

    initial commit for tut 5

---
+++ OpenGL_T5/OpenGL_T5/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_T5
+//
+//  Created by Lokesh Basu on 17/08/14.
+//  Copyright (c) 2014 Samsung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
