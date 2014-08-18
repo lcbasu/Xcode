b372d80f98c03006356b2cd9b6b5d62805017447
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-18 18:56:59 +0530

    cube transformation

---
+++ OpenGL_Ch3/OpenGL_Ch3/main.cpp
@@ -0,0 +1,58 @@
+//
+//  main.cpp
+//  OpenGL_Ch3
+//
+//  Created by Lokesh Basu on 18/08/14.
+//  Copyright (c) 2014 Samsung. All rights reserved.
+//
+
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+void init(void)
+{
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glShadeModel (GL_FLAT);
+}
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f (1.0, 1.0, 1.0);
+    glLoadIdentity ();
+    /* clear the matrix */
+    /* viewing transformation  */
+    gluLookAt (0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glScalef (1.0, 2.0, 1.0);
+    glutWireCube (1.0);
+    glFlush ();
+}
+
+void reshape (int w, int h)
+{
+    /* modeling transformation */
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+    glFrustum (-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+    glMatrixMode (GL_MODELVIEW);
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize (500, 500);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow (argv[0]);
+    init ();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
+    return 0;
+}
