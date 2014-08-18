36cc88a1a601e48e2d0ca2d55eec0d98ad6a86da
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-18 20:28:34 +0530

    solar system initial commit

---
+++ OpenGL_Ch3_Solar_System/OpenGL_Ch3_Solar_System/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_Ch3_Solar_System
+//
+//  Created by Lokesh Basu on 18/08/14.
+//  Copyright (c) 2014 Samsung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
