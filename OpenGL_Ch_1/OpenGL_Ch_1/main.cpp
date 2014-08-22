41e1aeb61e514650c035b5a0c28d8820a174d0c9
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-22 19:29:52 +0530

    initial commit for chapter 1 of redbook

---
+++ OpenGL_Ch_1/OpenGL_Ch_1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_Ch_1
+//
+//  Created by Lokesh Basu on 22/08/14.
+//  Copyright (c) 2014 Samsung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
