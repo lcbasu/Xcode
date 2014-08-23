e874f0f1dd9e6acfe422e1a3d0afe015ddf7a5dc
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-23 21:05:36 +0530

    including headers

--- OpenGL_Ch_1/OpenGL_Ch_1/main.cpp
+++ OpenGL_Ch_1/OpenGL_Ch_1/main.cpp
@@ -6,6 +6,10 @@
 //  Copyright (c) 2014 Samsung. All rights reserved.
 //
 
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <stdlib.h>
 #include <iostream>
 
 int main(int argc, const char * argv[])
