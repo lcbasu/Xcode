a5a96778a3cca02ea566fc68ce09221ae7fe1aa3
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-24 19:18:30 +0530

    initial commit for chapter 3

---
+++ OpenGL_Ch_3/OpenGL_Ch_3/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  OpenGL_Ch_3
+//
+//  Created by Lokesh Basu on 24/08/14.
+//  Copyright (c) 2014 Samsung. All rights reserved.
+//
+
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+void init()
+{
+    
+}
+
+void display()
+{
+    
+}
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
