a1f475079003b7f0dcecf869c3b8f61cddec0060
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-24 15:24:47 +0530

    solid torus

--- OpenGL_Ch_2_Ex_1/OpenGL_Ch_2_Ex_1/main.cpp
+++ OpenGL_Ch_2_Ex_1/OpenGL_Ch_2_Ex_1/main.cpp
@@ -11,5 +11,33 @@
 #include <OpenGL/glu.h>
 #include <stdlib.h>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+void init()
+{
+    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_LINE_LOOP);
+    glutSolidTorus(0.1f, 0.5f, 10.0f, 200.0f);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(600, 100);
+    glutCreateWindow("Solid Torus");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
