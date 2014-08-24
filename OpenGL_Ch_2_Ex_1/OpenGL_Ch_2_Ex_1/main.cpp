6ed24f73663e4f68e5353eff3472ffa769050985
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-24 14:19:18 +0530

    initial commit for chapter 2 example 1

---
+++ OpenGL_Ch_2_Ex_1/OpenGL_Ch_2_Ex_1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_Ch_2_Ex_1
+//
+//  Created by Lokesh Basu on 24/08/14.
+//  Copyright (c) 2014 Samsung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
