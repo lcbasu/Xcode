c7cf91ea04b4bc2ac70ccebb506e0808298aecf8
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-24 21:01:47 +0530

    chapter 3 example 1

--- OpenGL_Ch_3/OpenGL_Ch_3/main.cpp
+++ OpenGL_Ch_3/OpenGL_Ch_3/main.cpp
@@ -16,15 +16,39 @@ using namespace std;
 
 void init()
 {
-    
+    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    glShadeModel(GL_FLAT);
 }
 
 void display()
 {
-    
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glLoadIdentity();
+    gluLookAt(0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+    glScalef(1.0f, 1.0f, 1.0f);
+    glutWireCube(2.0f);
+    glFlush();
+}
+
+void reshape(int w, int h)
+{
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+    glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char *argv[])
 {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(400, 100);
+    glutCreateWindow("Chapter 3");
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
     return 0;
 }
