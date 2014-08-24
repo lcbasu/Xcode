bfc65acc0c04e2046e68ac7d1e4caa7ac59222c0
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-24 14:19:32 +0530

    include headers

--- OpenGL_Ch_2_Ex_1/OpenGL_Ch_2_Ex_1/main.cpp
+++ OpenGL_Ch_2_Ex_1/OpenGL_Ch_2_Ex_1/main.cpp
@@ -6,13 +6,10 @@
 //  Copyright (c) 2014 Samsung. All rights reserved.
 //
 
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <stdlib.h>
 #include <iostream>
 
-int main(int argc, const char * argv[])
-{
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
-
+using namespace std;
