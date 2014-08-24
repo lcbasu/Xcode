81bed03c9f4769996ab19439000324e342969131
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-24 10:24:31 +0530

    initial commit for double buffer example

---
+++ OpenGL_Ch_1_Double_Buffer/OpenGL_Ch_1_Double_Buffer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_Ch_1_Double_Buffer
+//
+//  Created by Lokesh Basu on 24/08/14.
+//  Copyright (c) 2014 Samsung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
