5871f452d210f4bde49eeaea2393c971f40678a7
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-24 11:29:42 +0530

    completes the spin display example

--- OpenGL_Ch_1_Double_Buffer/OpenGL_Ch_1_Double_Buffer/main.cpp
+++ OpenGL_Ch_1_Double_Buffer/OpenGL_Ch_1_Double_Buffer/main.cpp
@@ -6,13 +6,52 @@
 //  Copyright (c) 2014 Samsung. All rights reserved.
 //
 
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <stdlib.h>
 #include <iostream>
 
-int main(int argc, const char * argv[])
-{
+using namespace std;
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+static GLfloat _angle = 0.0f;
+
+void init()
+{
+    glClearColor(1.0f, 1.0f, 0.0f, 0.0f);
+    glShadeModel(GL_FLAT);
 }
 
+void dispaly()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPushMatrix();
+    glRotatef(_angle, 0.0f, 0.0f, 1.0f);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glRectf(-0.2f, -0.2f, 0.2f, 0.2f);
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+void spinDisplay()
+{
+    _angle += 1.0f;
+    if (_angle > 360) {
+        _angle -= 360;
+    }
+    glutPostRedisplay();
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(300, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Double Buffering");
+    init();
+    glutDisplayFunc(dispaly);
+    glutIdleFunc(spinDisplay);
+    glutMainLoop();
+    return 0;
+}
