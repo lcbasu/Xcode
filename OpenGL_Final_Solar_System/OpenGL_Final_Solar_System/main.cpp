c380f55122e17426266d3e5f24cc0221e14da5bc
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-28 19:05:33 +0530

    initial commit for final solar system

---
+++ OpenGL_Final_Solar_System/OpenGL_Final_Solar_System/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_Final_Solar_System
+//
+//  Created by Lokesh Basu on 28/08/14.
+//  Copyright (c) 2014 Samsung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
