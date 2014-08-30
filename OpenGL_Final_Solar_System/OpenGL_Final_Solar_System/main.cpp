7b8272a97957e4acebdc5fc11f639ad58c772345
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-30 16:53:17 +0530

    ambient

--- OpenGL_Final_Solar_System/OpenGL_Final_Solar_System/main.cpp
+++ OpenGL_Final_Solar_System/OpenGL_Final_Solar_System/main.cpp
@@ -53,6 +53,27 @@ GLuint LoadTexture(const char * filename, int width, int height)
     return texture;
 }
 
+
+void init()
+{
+    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat mat_shininess[] = { 50.0 };
+    GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+    
+    glShadeModel (GL_SMOOTH);
+    
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_DEPTH_TEST);
+}
+
+
+
+
 /*
 
 struct Material
@@ -187,19 +208,27 @@ void display (void)
     
     //g_SunLight.Activate();
     
-    
-    
     //Sunlight
     GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
     GLfloat mat_shininess[] = { 50.0 };
-    GLfloat light_position[] = { 1.0, 5.0, 0.0, 0.0 };
+    GLfloat light_position[] = { 0.0, 1.0, 0.0, 0.0 };
+    GLfloat light_ambient[] = { 0.5, 0.5, 0.5, 1.0 };
+    GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
     
     glShadeModel (GL_SMOOTH);
+    
     glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
     glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
+    glEnable(GL_DEPTH_TEST);
+
     
     //Draw Sun
     gluSphere(qObj, 1.0f, 24, 24);
@@ -307,7 +336,7 @@ void FreeTexture( GLuint texture )
 
 int main (int argc, char **argv) {
     glutInit (&argc, argv);
-    glutInitDisplayMode (GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
     
     glutInitWindowSize (500, 500);
     glutInitWindowPosition (100, 100);
