3335e636a12e66c1f1357e089c62f2fe6c733389
Lokesh Basu <lokesh.basu@gmail.com>
2014-09-03 21:03:25 +0530

    initial commit for OpenGL ES

---
+++ OpenGL_ES/OpenGL_ES/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_ES
+//
+//  Created by Lokesh Basu on 03/09/14.
+//  Copyright (c) 2014 Samsung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
