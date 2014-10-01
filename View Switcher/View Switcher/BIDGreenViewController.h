5ce69ff0cd160f68410fbd77e78840bd3b2b43b3
Lokesh Basu <lokesh.basu@gmail.com>
2014-10-02 05:00:41 +0530

    adds a green view controller

---
+++ View Switcher/View Switcher/BIDGreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDGreenViewController.h
+//  View Switcher
+//
+//  Created by Lokesh Basu on 02/10/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDGreenViewController : UIViewController
+
+@end
