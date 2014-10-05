01892ffb5d02d2e6d8a6691c183185a97816fd10
Lokesh Basu <lokesh.basu@gmail.com>
2014-10-05 07:00:26 +0530

    creating file and view controller for view

---
+++ FailedBanks/FailedBanks/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  FailedBanks
+//
+//  Created by Lokesh Basu on 05/10/14.
+//  Copyright (c) 2014 IIT Roorkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
