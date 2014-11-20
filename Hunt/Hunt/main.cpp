ea9a3c2382296a6b7553bb8765cff34bf0e2616b
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-20 21:50:28 +0530

    debug the HR code

--- Hunt/Hunt/main.cpp
+++ Hunt/Hunt/main.cpp
@@ -20,47 +20,27 @@ using namespace std;
 
 int main()
 {
-    int N;
-    cin >> N;
-    int *arr = new int[N];
-    int *outArr = new int[N];
-    int *count = new int[100];
+    int T;
     
-    for(int i = 0; i < 100; i++)
+    cin >> T;
+    
+    while(T--)
     {
-        count[i] = 0;
+        int N;
+        
+        cin >> N;
+        
+        int *arr = new int[N];
+        
+        for(int i = 0; i < N; i++)
+        {
+            int tmp;
+            cin >> tmp;
+            arr[i] = tmp;
+        }
+        
+        
     }
-    
-    for(int i = 0; i < N; i++)
-    {
-        int tmp;
-        cin >> tmp;
-        arr[i] = tmp;
-        count[tmp] = count[tmp] + 1;
-    }
-    
-    for(int i = 1; i < 100; i++)
-    {
-        count[i] = count[i-1] + count[i];
-    }
-    
-    for(int i = N-1; i >= 0; i--)
-    {
-        int finalIndex = count[arr[i]];
-        outArr[finalIndex] = arr[i];
-        count[arr[i]] = count[arr[i]] -1;
-    }
-    
-    for(int i = 0; i < N; i++)
-    {
-        cout << outArr[i] << " ";
-    }
-    
-    cout << endl;
-    
-    
-    
-    
     
     return 0;
 }
