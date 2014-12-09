8d2eb25d25163b2b7f3f0452abc4788f3aaf35ed
Lokesh Basu <lokesh.basu@gmail.com>
2014-12-09 21:06:48 +0530

    project setup for OpenGL Programming

---
+++ OpenGLProgramming/OpenGLProgramming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLProgramming
+//
+//  Created by Lokesh Basu on 09/12/14.
+//  Copyright (c) 2014 InHouse. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
