cac4013a68e7899689094bb92d9275c4c7592dc6
Lokesh Basu <lokesh.basu@gmail.com>
2015-01-22 19:49:37 +0530

    rotation

--- OpenGLProgramming/OpenGLProgramming/main.cpp
+++ OpenGLProgramming/OpenGLProgramming/main.cpp
@@ -34,6 +34,7 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
     glTranslatef(0, 0, -10);
+    glRotatef(45, 0, 1, 0);
     glBegin(GL_TRIANGLES);
     glColor3f(1.0, 0.0, 0.0);
     glVertex3f(0, 1, -5);
