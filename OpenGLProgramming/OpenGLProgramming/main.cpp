5fb95ccbd0f9f96515778ffa55f80c61199a3d83
Lokesh Basu <lokesh.basu@gmail.com>
2015-01-30 19:00:15 +0530

    refactoring

--- OpenGLProgramming/OpenGLProgramming/main.cpp
+++ OpenGLProgramming/OpenGLProgramming/main.cpp
@@ -29,11 +29,6 @@ float _angle = 0;
 float _scaleValue = 0.1;
 GLuint brickTexture;
 
-//float pos[] = {-2, 2.0, -3, 1};
-//float dif[] = {1, 1, 1, 1};
-//float amb[] = {0.1, 0.1, 0.1, 1};
-//float spe[] = {1, 1, 1, 1};
-
 struct coordinate {
     float x, y, z;
     coordinate(float a, float b, float c) : x(a), y(b), z(c) {};
@@ -51,61 +46,6 @@ struct face {
         four = true;
     }
 };
-
-
-
-
-//void drawCube(float size)
-//{
-//    //material diffuse and ambient color
-//    float difamb[] = {1, 0.5, 0.3, 1};
-//    glBegin(GL_QUADS);
-//    //applying material to front and back side
-//    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, difamb);
-//    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, difamb);
-//    //front face
-//    glNormal3f(0, 0, 1);
-//    glVertex3f(size/2, size/2, size/2);
-//    glVertex3f(-size/2, size/2, size/2);
-//    glVertex3f(-size/2, -size/2, size/2);
-//    glVertex3f(size/2, -size/2, size/2);
-//    
-//    //left face
-//    glNormal3f(-1, 0, 0);
-//    glVertex3f(-size/2, size/2, size/2);
-//    glVertex3f(-size/2, size/2, -size/2);
-//    glVertex3f(-size/2, -size/2, -size/2);
-//    glVertex3f(-size/2, -size/2, size/2);
-//    
-//    //back face
-//    glNormal3f(0, 0, -1);
-//    glVertex3f(size/2, size/2, -size/2);
-//    glVertex3f(-size/2, size/2, -size/2);
-//    glVertex3f(-size/2, -size/2, -size/2);
-//    glVertex3f(size/2, -size/2, -size/2);
-//    
-//    //right face
-//    glNormal3f(1, 0, 0);
-//    glVertex3f(size/2, size/2, -size/2);
-//    glVertex3f(size/2, size/2, size/2);
-//    glVertex3f(size/2, -size/2, size/2);
-//    glVertex3f(size/2, -size/2, -size/2);
-//
-//    //top face
-//    glNormal3f(0, 1, 0);
-//    glVertex3f(size/2, size/2, size/2);
-//    glVertex3f(-size/2, size/2, size/2);
-//    glVertex3f(-size/2, size/2, -size/2);
-//    glVertex3f(size/2, size/2, -size/2);
-//    
-//    //bottom face
-//    glNormal3f(0, -1, 0);
-//    glVertex3f(size/2, -size/2, size/2);
-//    glVertex3f(-size/2, -size/2, size/2);
-//    glVertex3f(-size/2, -size/2, -size/2);
-//    glVertex3f(size/2, -size/2, -size/2);
-//    glEnd();
-//}
 
 int loadObject(const char* fileName)
 {
@@ -209,54 +149,11 @@ void init()
     gluPerspective(45, 640/480, 1, 50);
     glMatrixMode(GL_MODELVIEW);
     glEnable(GL_DEPTH_TEST);
-    glEnable(GL_TEXTURE_2D);
-    const char * filename = "/Users/LokeshBasu/Documents/Xcode/OpenGLProgramming/OpenGLProgramming/brickwork-texture_opengl.bmp";
-    brickTexture = LoadTexture(filename, 512, 512);
-//    glEnable(GL_LIGHTING);
-//    glEnable(GL_LIGHT0);
-//
-//    glLightfv(GL_LIGHT0, GL_DIFFUSE, dif);
-//    glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
-//    glLightfv(GL_LIGHT0, GL_SPECULAR, spe);
 }
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-//    glLightfv(GL_LIGHT0, GL_POSITION, pos);
-    glTranslatef(0, 0, -5);
-    glRotatef(_angle, 1, 1, 1);
-//    glScalef(_scaleValue, _scaleValue, _scaleValue);
-//    drawCube(1);
-    
-    
-    glBindTexture(GL_TEXTURE_2D, brickTexture);
-    glBegin(GL_QUADS);
-    glVertex3f(-1, 1, 0);
-    glTexCoord2f(0, 0);
-    glVertex3f(-1, -1, 0);
-    glTexCoord2f(0, 3);
-    glVertex3f(1, -1, 0);
-    glTexCoord2f(3, 3);
-    glVertex3f(1, 1, 0);
-    glTexCoord2f(3, 0);
-    glEnd();
-    glFlush();
-}
-
-void update(int value)
-{
-    _angle += 0.5;
-    _scaleValue += 0.1;
-    if (_scaleValue > 2) {
-        _scaleValue = 0.1;
-    }
-    if (_angle > 360) {
-        _angle -= 360;
-    }
-    glutPostRedisplay();
-    glutTimerFunc(25, update, 0);
 }
 
 int main(int argc, char* argv[]) {
@@ -269,8 +166,6 @@ int main(int argc, char* argv[]) {
     init();
     glutDisplayFunc (display);
     glutIdleFunc(display);
-    glutTimerFunc(25, update, 0);
-    
     glutMainLoop ();
     
     return 0;
